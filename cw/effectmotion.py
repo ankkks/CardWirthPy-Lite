bfba7286b4633248becc8b510eb7f779e9d79b67
akkw <ankkkcw@excite.co.jp>
2018-01-01 02:10:28 +0900

    ????????????-32768~32767????(CW??)

--- cw/effectmotion.py
+++ cw/effectmotion.py
@@ -1023,7 +1023,7 @@ class EffectMotion(object):
         """
         if success_res:
             return False
-        value = cw.util.numwrap(self.value, -100, 100)
+        value = cw.util.numwrap(self.value, -32768, 32767)
         if value <> 0:
             duration = self.calc_durationvalue(target, True)
         else:
@@ -1041,7 +1041,7 @@ class EffectMotion(object):
         """
         if success_res:
             return False
-        value = cw.util.numwrap(self.value, -10, 10)
+        value = cw.util.numwrap(self.value, -32768, 32767)
         if value <> 0:
             duration = self.calc_durationvalue(target, True)
         else:
@@ -1059,7 +1059,7 @@ class EffectMotion(object):
         """
         if success_res:
             return False
-        value = cw.util.numwrap(self.value, -10, 10)
+        value = cw.util.numwrap(self.value, -32768, 32767)
         if value <> 0:
             duration = self.calc_durationvalue(target, True)
         else:
@@ -1077,7 +1077,7 @@ class EffectMotion(object):
         """
         if success_res:
             return False
-        value = cw.util.numwrap(self.value, -10, 10)
+        value = cw.util.numwrap(self.value, -32768, 32767)
         if value <> 0:
             duration = self.calc_durationvalue(target, True)
         else:
